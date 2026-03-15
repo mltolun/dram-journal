82ab8d95caff76cf42a3c005174b0286cc0e62f7
Martin Luna <martin.luna@gmail.com>
2026-03-15 21:34:07 +0100

    fix: set base URL to / for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   // Replace 'dram-journal' with your actual GitHub repo name
-  base: process.env.NODE_ENV === 'production' ? '/dram-journal' : '/',
+  base: '/',
 })
