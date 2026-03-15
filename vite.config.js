cbaecd1d3b2338952cc0c7468fd33f417386fc2c
Martin Luna <martin.luna@gmail.com>
2026-03-15 23:34:28 +0100

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  // Replace 'dram-journal' with your actual GitHub repo name
-  base: process.env.NODE_ENV === 'production' ? '/dram-journal' : '/',
+  base: process.env.VITE_BASE_PATH || '/',
 })
